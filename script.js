d72f961c2d64d68cb0f25aec810ca5ac84f2b1d2
toma-s <toma.savkova@gmail.com>
2020-10-31 15:46:31 +0100

    Revert triangle coords changes

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ function setCanvasSize() {
     canvasElement.style.display = 'block';
 }
 
+
+
 function setSpiral() {
     if (document.getElementById('canvasSpiral')) {
         return;
@@ -55,6 +57,21 @@ function setCircle() {
     currentSketch = new p5(circleSketch, element);
 }
 
+function setX() {
+    if (document.getElementById('canvasX')) {
+        return;
+    }
+
+    clearUpCanvas();
+
+    let element = document.createElement('div');
+    element.id = 'canvasX';
+    window.document.getElementById('canvas').appendChild(element);
+    currentSketch = new p5(sketchX, element);
+}
+
+
+
 function clearUpCanvas() {
     if (currentSketch) {
         currentSketch.remove();
@@ -65,71 +82,7 @@ function clearUpCanvas() {
     }
 }
 
-let triangleSketch = function(f) {
-    let angle = 0;
-    let degreesMode = true;
 
-    f.setup = function() {
-        f.createCanvas(canvasWidth, canvasHeight);
-        f.background('#292929');
-        f.frameRate(30);
-        f.noFill();
-        f.angleMode(f.DEGREES);
-    }
-
-    f.draw = function() {
-        if (degreesMode) {
-            f.angleMode(f.DEGREES);
-            f.stroke(f.random(200,230), f.random(200,250), f.random(0));
-        } else {
-            f.angleMode(f.RADIANS);
-            f.stroke('#292929');
-        }
-        f.translate(canvasWidth / 2, canvasHeight / 2);
-        f.rotate(angle);
-
-        f.triangle(-200, 200, 0, 0, 200, 200);
-
-        angle += 1 % 360;
-        if (angle > 360) {
-            angle = 0;
-            degreesMode = !degreesMode;
-        }
-    }
-}
-
-let circleSketch = function(f) {
-    let angle = 0;
-    let degreesMode = true;
-
-    f.setup = function() {
-        f.createCanvas(canvasWidth, canvasHeight);
-        f.background('#292929');
-        f.frameRate(30);
-        f.noFill();
-        f.angleMode(f.DEGREES);
-    }
-
-    f.draw = function() {
-        if (degreesMode) {
-            f.angleMode(f.DEGREES);
-            f.stroke(f.random(210,250), f.random(30), f.random(50, 100));
-        } else {
-            f.angleMode(f.RADIANS);
-            f.stroke('#292929');
-        }
-        f.translate(canvasWidth / 2, canvasHeight / 2);
-        f.rotate(angle);
-
-        f.triangle(-200, 200, 0, 0, 200, 200);
-
-        angle += 1 % 360;
-        if (angle > 360) {
-            angle = 0;
-            degreesMode = !degreesMode;
-        }
-    }
-}
 
 let spiralSketch = function(f) {
     let x = canvasWidth / 2;
@@ -172,4 +125,82 @@ let spiralSketch = function(f) {
     f.rotateRight = function() {
         currentAngle += angle % 360;
     };
-};
+}
+
+let triangleSketch = function(f) {
+    let angle = 0;
+    let degreesMode = true;
+
+    f.setup = function() {
+        f.createCanvas(canvasWidth, canvasHeight);
+        f.background('#292929');
+        f.frameRate(30);
+        f.noFill();
+        f.angleMode(f.DEGREES);
+    }
+
+    f.draw = function() {
+        if (degreesMode) {
+            f.angleMode(f.DEGREES);
+            f.stroke(f.random(200,230), f.random(200,250), f.random(0));
+        } else {
+            f.angleMode(f.RADIANS);
+            f.stroke('#292929');
+        }
+        f.translate(canvasWidth / 2, canvasHeight / 2);
+        f.rotate(angle);
+
+        f.triangle(-160, 160, 0, -160, 160, 160);
+
+        angle += 1 % 360;
+        if (angle > 360) {
+            angle = 0;
+            degreesMode = !degreesMode;
+        }
+    }
+}
+
+let circleSketch = function(f) {
+    let angle = 0;
+    let degreesMode = true;
+
+    f.setup = function() {
+        f.createCanvas(canvasWidth, canvasHeight);
+        f.background('#292929');
+        f.frameRate(30);
+        f.noFill();
+        f.angleMode(f.DEGREES);
+    }
+
+    f.draw = function() {
+        if (degreesMode) {
+            f.angleMode(f.DEGREES);
+            f.stroke(f.random(210,250), f.random(30), f.random(50, 100));
+        } else {
+            f.angleMode(f.RADIANS);
+            f.stroke('#292929');
+        }
+        f.translate(canvasWidth / 2, canvasHeight / 2);
+        f.rotate(angle);
+
+        f.triangle(-200, 200, 0, 0, 200, 200);
+
+        angle += 1 % 360;
+        if (angle > 360) {
+            angle = 0;
+            degreesMode = !degreesMode;
+        }
+    }
+}
+
+let sketchX = function(f) {
+    f.setup = function() {
+        f.createCanvas(canvasWidth, canvasHeight);
+        f.background('#292929');
+        f.frameRate(30);
+    };
+    
+    f.mousePressed = function() {
+        f.point(f.mouseX, f.mouseY);
+    }
+}
